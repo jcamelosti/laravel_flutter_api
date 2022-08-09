7786bf6c1fbb418c97059ef188e45f0e6c5ee646
Josué Camelo <cloudone@josuecamelo.com>
2022-08-09 00:31:32 -0300

    Autenticação para Obter Token JWT Bearer

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+
+//API route for register new user
+Route::post('/register', [App\Http\Controllers\API\UserController::class, 'register']);
+//API route for login user
+Route::post('/login', [App\Http\Controllers\API\UserController::class, 'login']);
